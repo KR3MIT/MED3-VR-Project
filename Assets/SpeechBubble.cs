e0f8c43d094119a09281b7430e88492f01df8a6d
KR3MIT <jason.cm.hassan@gmail.com>
2024-10-04 11:25:46 +0200

    speech bubble

---
+++ Assets/SpeechBubble.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubble : MonoBehaviour
+{
+    [SerializeField] private GameObject text;
+    [SerializeField] private AICompanion ai;
+    [SerializeField] private Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        text.transform.LookAt(player);
+    }
+}
