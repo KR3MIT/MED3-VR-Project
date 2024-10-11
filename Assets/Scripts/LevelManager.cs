7ce2a0dd4ce41d609e6399952be524c3b9c35718
m4tt <33161422+m4ttxdd@users.noreply.github.com>
2024-10-11 10:56:53 +0200

    Update LevelManager.cs

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -25,17 +25,6 @@ public class LevelManager : MonoBehaviour
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    private void Start()
-    {
-        StartCoroutine(test());
-    }
-
-    IEnumerator test ()
-    {
-        yield return new WaitForSeconds(3f);
-        EndLevel(3f, "lvl 1");
-    }
-
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         SceneTransition.instance.FadeIn(fadeInTime);
