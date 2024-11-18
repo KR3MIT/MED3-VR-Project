4ab534fb812f28b19c657d5e54215d915d7fe7bc
m4tt <33161422+m4ttxdd@users.noreply.github.com>
2024-11-18 10:36:24 +0100

    fuuuck :9

--- Assets/ShowCurrentSign.cs
+++ Assets/ShowCurrentSign.cs
@@ -2,21 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class ShowCurrentSign : MonoBehaviour
 {
-    public TMP_Text text;
+    public TMP_Text tmptext;
+    public Text unitytext;
 
     private string originalText;
 
 
     private void Start()
     {
-        originalText = text.text;
+        if (unitytext == null)
+        {
+            originalText = tmptext.text;
+        }
+        else if (tmptext == null)
+        {
+            originalText = unitytext.text;
+        }
     }
 
-    public void UpdateText(string sign)
+public void UpdateText(string sign)
     {
-        text.text = originalText + sign;
+        if(tmptext == null)
+        {
+            unitytext.text = originalText + sign;
+            return;
+        }
+
+        if(unitytext == null)
+        {
+            tmptext.text = originalText + sign;
+            return;
+        }
     }
 }
